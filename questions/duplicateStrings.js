2b1423a987ff07905978fb3ba9cb819016bb1ed9
Joshua Mutiso <67162405+Mozzie455@users.noreply.github.com>
2023-08-22 22:54:15 +0300

    Update duplicateStrings.js

--- questions/duplicateStrings.js
+++ questions/duplicateStrings.js
@@ -5,3 +5,11 @@
  const str = 'This is is a test test string';
  removeDuplicates(str); // 'This is a test string'
  */
+function removeDuplicates(str){
+ const arr = str.split(' ');
+
+ const set = new Set(arr);
+ const newString = [...set].join(' ');
+
+ return newString;
+}
