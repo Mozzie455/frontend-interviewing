03708a2f153f2a7909e10fdaf309db697a4c1a1c
Joshua Mutiso <67162405+Mozzie455@users.noreply.github.com>
2023-08-23 22:50:32 +0300

    Update moveElement.js

--- questions/moveElement.js
+++ questions/moveElement.js
@@ -4,3 +4,18 @@
 /*
     function moveElement(duration, distance, element) {}
 */
+function moveElement(duration, distance, element) {
+    const start = performance.now();
+    function move(currentTime){
+        const elapsed = currentTime - start;
+        const progress = elapsed / duration;
+        const amountToMove = progress * distance;
+        
+        element.style.transform = `translateX(${ amountToMove}px)`;
+        if ( amountToMove < distance){
+            requestAnimationFrame(move);
+        }
+    }
+
+    requestAnimationFrame();
+}
