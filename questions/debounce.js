d1f6623a5cd59581e2a1309222b5a35660d1a9fa
Joshua Mutiso <67162405+Mozzie455@users.noreply.github.com>
2023-08-23 19:27:52 +0300

    Update debounce.js

--- questions/debounce.js
+++ questions/debounce.js
@@ -1,1 +1,14 @@
 // Implement debounce
+
+function debounce(fn, time){
+	return function(){
+		if (setTimeoutId){
+			clearTimeout(setTimeoutId);
+		}
+		setTimeoutId = setTimeout(() => {
+			fn.apply(this, arguments);
+			setTimeoutId = null;
+		}, time);
+	}
+}
+
